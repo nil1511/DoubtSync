4e70b2988efaf5bcb5aa34927c0128104499fdd2
Nilesh Suthar <nileshsuthar@live.in>
2014-02-26 16:02:08 +0530

    added registration and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Doubtsync::Application.routes.draw do
-  get "auth/login"
-  get "auth/register"
-  devise_for :users
+  devise_for :users, path: "auth", path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
