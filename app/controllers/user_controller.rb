daaa0dd31f5dcdb79a627c88792b4e77c4dc2465
Nilesh <nil.15111993@gmail.com>
2014-03-23 19:58:51 +0530

    profile update

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -25,7 +25,8 @@ class UserController < ApplicationController
 	end
 	def manage
 		if current_user.role.name.eql? "student"
-			student = Student.find_by_id()
+			@student = Student.find_by user_id: current_user.id
+			
 			render 'profile'
 		else
 			render :text => current_user.role.name
@@ -33,17 +34,27 @@ class UserController < ApplicationController
 	end
 	def save
 		#TODO Check validation
+		#FIXME Dry
 		if current_user.role.name.eql? "student"
 			if !Student.find_by user_id: current_user.id?
 				dob = params['user']
 				mobile = params['mobile']
+				gender = params['gender'].eql?"male"
 				student = Student.new(:first_name => params['first_name'],:last_name => params['last_name'],
-					:degree => params['degree'],:graduate_year => params['graduate'],:gender => params['gender'],
+					:degree => params['degree'],:graduate_year => params['graduate'],:gender => gender,
 					:dob => dob['dob'],:mobile => mobile['phone'],:user_id => current_user.id)
 				student.save
 					render :text =>  "saved"
 			else
-				render :text =>  "noththing something"
+				student = current_user.student
+				dob = params['user']
+				mobile = params['mobile']
+				gender = params['gender'].eql?"male"
+				student.update(:first_name => params['first_name'],:last_name => params['last_name'],
+					:degree => params['degree'],:graduate_year => params['graduate'],:gender => gender,
+					:dob => dob['dob'],:mobile => mobile['phone'])
+				student.save
+				render :text =>  "updated"
 			end
 		else
 			render :text =>  "TODO"+current_user.role.name
