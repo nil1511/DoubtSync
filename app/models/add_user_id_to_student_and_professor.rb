aeabb1dfaf8a6c88137e0b2cbb99a81e974a1f13
Nilesh <nil.15111993@gmail.com>
2014-03-27 02:01:39 +0530

    removed unwanted files

--- app/models/add_user_id_to_student_and_professor.rb
+++
@@ -1,2 +0,0 @@
-class AddUserIdToStudentAndProfessor < ActiveRecord::Base
-end
--- test/models/add_user_id_to_student_and_professor_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AddUserIdToStudentAndProfessorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
