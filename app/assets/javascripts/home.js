2f5e634398d67d4147b960acbfa0a59e11978b4e
Nilesh <nil.15111993@gmail.com>
2014-03-27 17:13:20 +0530

    Added login client validation

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,5 +1,12 @@
 $(function(){
-	$('.btn.register').on('click',function (event) {
-		
+	$('.btn.login').on('click',function (event) {
+		if($('#user_email').val()==''){
+			$('#user_email').parent('.form-group').addClass('has-error')
+			return false;
+		}
+		if($('#user_password').val()==''){
+			$('#user_password').parent('.form-group').addClass('has-error')
+			return false;
+		}
 	})
 });
