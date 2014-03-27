c781c197f8fa4723294f4b892b13f2d7f6c9fa8c
Nilesh <nil.15111993@gmail.com>
2014-03-27 19:11:08 +0530

    Loggedin url redirect

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
 	layout 'home'
 	def index
-		
+		if current_user
+			redirect_to '/main/feed'
+		end
 	end
 end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,11 +1,11 @@
 class MainController < ApplicationController
 	before_filter :authenticate_user!
   def index
-  	
+  	redirect_to :action => 'feed'
   end
   # TODO add client size validation for Registeration and then login
   def feed
   	#FIXME Check header's nav bar on corresponding erb
-  	@posts = Post.from_users_followed_by(current_user)
+  	@posts = Post.from_users_followed_by(current_user)  	
   end
 end
