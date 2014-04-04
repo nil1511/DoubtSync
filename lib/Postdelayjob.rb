2aa743b8c751a79c118b9db1550a482e67d649ac
Nilesh <nil.15111993@gmail.com>
2014-04-04 11:33:33 +0530

    Fixed post new topic

--- lib/Postdelayjob.rb
+++ lib/Postdelayjob.rb
@@ -1,6 +1,5 @@
 class NotifyPostaddTopic < Struct.new(:post_id)
 
-	#FIXME New Topic is not adapted by post
 	def perform
 		post = Post.find_by_id(post_id)
 		puts "Started job"
@@ -15,21 +14,17 @@ class NotifyPostaddTopic < Struct.new(:post_id)
 			end
 		end
 		puts "Added Notification";
-
-
 		post.text.split('#').each_with_index do |item,index|
 			if index == 0
 				next
 			end
 			n = item.split(' ')[0]
 			topic = Topic.create(name: n);
-			post.htags = post.htags.to_s + topic.id.to_s+',';
-			post.save
-			puts topic.id.to_s
+			post.htags = post.htags.to_s +','+ topic.id.to_s;
 		end
-		
+		post.save
+	
 		htags = post.htags.split(',');
-		puts htags.to_s;
 		htags.each do |topic_id|
 			if topic_id.to_i != 0
 				TopicPost.create(topic_id: topic_id,post_id: post_id);
