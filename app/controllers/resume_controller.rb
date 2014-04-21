3a9fb9264071ae1db441bf8a27e529aa48cd6cb8
Nilesh <nil.15111993@gmail.com>
2014-04-21 21:19:45 +0530

    Added My Topics listing

--- app/controllers/resume_controller.rb
+++ app/controllers/resume_controller.rb
@@ -1,4 +1,10 @@
 class ResumeController < ApplicationController
   def daiict
+  	@full_name = "Nilesh Suthar"
+  	@college_idcard_id = '201101147'
+  	@month = "Novemeber"
+  	@day = 15
+  	@year = 1993
+
   end
 end
