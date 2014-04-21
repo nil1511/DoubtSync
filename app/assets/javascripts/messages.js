4662f29670955691c8c3fa2081abeb31e17ae6e3
Nilesh <nil.15111993@gmail.com>
2014-04-21 12:34:49 +0530

    Message now sent via reply btn

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,2 +1,20 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(function(){
+
+	$('.reply').on('click',function(){
+		var btn =$(this);
+		var textarea = btn.prev().prev('textarea');
+		
+	if(textarea.val().trim()!==''){
+		data = {};
+		data.text = textarea.val();
+		$.post('/message/'+btn.data('userid'),{message: JSON.stringify(data)},function(res){			
+			if(res==btn.data('userid')){
+				$('#messagelist'+res).append('<div class="messagetext">From me: '+ textarea.val() +'at now</div>')
+				
+				textarea.val('');
+			}
+		})
+		}
+	});
+});
+
