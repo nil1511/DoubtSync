76a89df36e428906e5a7eda003501a156055009f
Nilesh Suthar <nileshsuthar@live.in>
2016-01-14 01:46:42 +0530

    update mysql2 gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,19 +30,20 @@ Doubtsync::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
- 
+
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  address: 'smtp.gmail.com',
-  port: 587,
-  domain: 'gmail.com',
-  user_name: 'doubtsync@gmail.com',
-  password: '@sen@Password',
-  authentication: 'plain',
-  enable_starttls_auto: true }
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: 'doubtsync@gmail.com',
+    password: '@sen@Password',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   config.action_mailer.raise_delivery_errors = true
 
-  
+
 end
